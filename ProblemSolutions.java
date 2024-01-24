2e3127c7c74ef910bfe0df9e934bf6b72bd6c621
DouglasMB <157315318+D0ugla5MB@users.noreply.github.com>
2024-01-23 22:10:49 -0300

    new solution for the Merge Strings Alternately
    I include an if condition when the Strings (words 1 and 2) are equal because I would not to check always their' sizes.

--- ProblemSolutions.java
+++ ProblemSolutions.java
@@ -3,26 +3,37 @@ public class ProblemSolutions implements ProblemList {
     @Override
     public String mergeAlternately(String word1, String word2) {
 
-        /* if (word1.length() <= 0 && word2.length() <= 0) {
-            return "";
-        }
-        if (word1 == "" || word1.length() == 0) {
-            return word2;
-        }
-        if (word2 == "" || word2.length() == 0) {
-            return word1;
-        } */
+        /*
+         * if (word1.length() <= 0 && word2.length() <= 0) {
+         * return "";
+         * }
+         * if (word1 == "" || word1.length() == 0) {
+         * return word2;
+         * }
+         * if (word2 == "" || word2.length() == 0) {
+         * return word1;
+         * }
+         */
 
-        int length = word1.length() + word2.length();
-        char[] newString = new char[length];
+        int newSize = word1.length() + word2.length();
+        char[] newString = new char[newSize];
+        int j = 0;
 
-        int index = 0;
-        for (int i = 0; i < length; i++) {
-            if (i < word1.length()) {
-                newString[index++] = word1.charAt(i);
+        if (word1.length() == word2.length()) {
+            newSize = newString.length / 2;
+            j = -1;
+            for (int i = 0; i < newSize; i++) {
+                newString[2 * i] = word1.charAt(i);
+                newString[j += 2] = word2.charAt(i);
             }
-            if (i < word2.length()) {
-                newString[index++] = word2.charAt(i);
+        } else {
+            for (int i = 0; i < newSize; i++) {
+                if (i < word1.length()) {
+                    newString[j++] = word1.charAt(i);
+                }
+                if (i < word2.length()) {
+                    newString[j++] = word2.charAt(i);
+                }
             }
         }
 
